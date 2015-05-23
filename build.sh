4d4b60f592c788ab23258115ac4b2687854b23fd
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-23 23:38:58 +0200

    Added build.sh.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script will send the source files to the server, tell said server to compile the source files
+# , and retrieve the executable file.
+
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ root@188.226.217.13:/root/Projects/tribal/ && ssh root@188.226.217.13 cd Projects/tribal/ && scons
+
+rsync -arvzSH --include="No-Motivation" --exclude="*" root@188.226.217.13:/root/Projects/tribal/ ~/Projects/tribal/ && ~/Projects/tribal/run.sh
+
