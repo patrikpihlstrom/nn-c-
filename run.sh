06e3ffaeae1783ad96d9fbf82f1930cd1f1bad96
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-24 00:03:55 +0200

    Better comments for build.sh.

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-__GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
+#!/bin/bash
+
+# Retrieve executable and execute said executable.
+rsync -arvzSH --include="No-Motivation" --exclude="*" root@188.226.217.13:/root/Projects/tribal/ ~/Projects/tribal/ && __GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
+
