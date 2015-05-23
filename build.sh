8430fc57acaccaecab931930f63f0098d27c4d94
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-23 23:57:22 +0200

    Fixed scons command.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 echo "Scons option: "
 read option
 
-rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ root@188.226.217.13:/root/Projects/tribal/ && ssh root@188.226.217.13 cd Projects/tribal/ && scons $option
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ root@188.226.217.13:/root/Projects/tribal/ && ssh root@188.226.217.13 "cd Projects/tribal/; scons $option"
 
 rsync -arvzSH --include="No-Motivation" --exclude="*" root@188.226.217.13:/root/Projects/tribal/ ~/Projects/tribal/ && ~/Projects/tribal/run.sh
 
