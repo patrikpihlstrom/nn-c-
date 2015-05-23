87b02e47c2a90fd33fe6217170d816ca542e65a4
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-23 23:41:57 +0200

    Added scons option to build.sh.

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # This script will send the source files to the server, tell said server to compile the source files
 # , and retrieve the executable file.
 
-rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ root@188.226.217.13:/root/Projects/tribal/ && ssh root@188.226.217.13 cd Projects/tribal/ && scons
+echo "Scons option: "
+read option
+
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ root@188.226.217.13:/root/Projects/tribal/ && ssh root@188.226.217.13 cd Projects/tribal/ && scons $option
 
 rsync -arvzSH --include="No-Motivation" --exclude="*" root@188.226.217.13:/root/Projects/tribal/ ~/Projects/tribal/ && ~/Projects/tribal/run.sh
 
