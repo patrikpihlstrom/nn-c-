6979cde0da72041522e66bb277d62c91109361f6
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-24 11:50:08 +0200

    Can now run run.sh without a network connection.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Retrieve executable and execute said executable.
-rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@188.226.217.13:~/Projects/tribal/ ~/Projects/tribal/ && __GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
+rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@188.226.217.13:~/Projects/tribal/ ~/Projects/tribal/
+__GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
 
