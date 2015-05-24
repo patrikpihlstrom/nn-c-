8346389529a609c73fffdbff5169961f57750613
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-24 03:33:32 +0200

    Changed user in build.sh and run.sh.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ printf "Scons option: "
 read option
 
 # Send src and compile.
-rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ root@188.226.217.13:/root/Projects/tribal/ && ssh root@188.226.217.13 "cd Projects/tribal/; scons $option"
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@188.226.217.13:~/Projects/tribal/ && ssh cognizance@188.226.217.13 "cd Projects/tribal/; scons $option"
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Retrieve executable and execute said executable.
-rsync -arvzSH --include="No-Motivation" --exclude="*" root@188.226.217.13:/root/Projects/tribal/ ~/Projects/tribal/ && __GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
+rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@188.226.217.13:~/Projects/tribal/ ~/Projects/tribal/ && __GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
 
