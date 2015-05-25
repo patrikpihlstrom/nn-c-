e848902f1f56c25eff59e859e6e87149a7048431
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-05-25 07:26:39 +0200

    Specify host for building and running.

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+printf "Host: "
+read host
+
 printf "Scons option: "
 read option
 
 # Send src and compile.
-rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@188.226.217.13:~/Projects/tribal/ && ssh cognizance@188.226.217.13 "cd Projects/tribal/; scons $option"
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@$host:~/Projects/tribal/ && ssh cognizance@$host "cd Projects/tribal/; scons $option"
 
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+printf "Host: "
+read host
+
 # Retrieve executable and execute said executable.
-rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@188.226.217.13:~/Projects/tribal/ ~/Projects/tribal/
+rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@$host:~/Projects/tribal/ ~/Projects/tribal/
 __GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
 
