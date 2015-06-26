22c59f0710bea45f98af044524a4a70e81bebd2e
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-06-26 02:36:54 +0200

    Only one job for test.sh.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "Building on Luke."
-rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@Luke:~/Projects/tribal/ && ssh cognizance@Luke "cd Projects/tribal/; scons -j8"
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@Luke:~/Projects/tribal/ && ssh cognizance@Luke "cd Projects/tribal/; scons"
 
 printf "Retrieving the executable from Luke and running."
 rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@Luke:~/Projects/tribal/ ~/Projects/tribal/
