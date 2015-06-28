9f27bfe0dd3aa342a9147fce279d9d3124907cc1
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2015-06-28 03:06:05 +0200

    Updated shell scripts to use the new build system.

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ read host
 
 # Retrieve executable and execute said executable.
 rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@$host:~/Projects/tribal/ ~/Projects/tribal/
-__GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
+__GL_SYNC_TO_VBLANK=0 primusrun ./No-Motivation
 
--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 printf "Building on Luke."
-rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@Luke:~/Projects/tribal/ && ssh cognizance@Luke "cd Projects/tribal/; scons"
+rsync -arvzSH --include="*.cpp" --include="*.hpp" --include="SConstruct" --include="*.lua" --include="*/" --exclude="*" ~/Projects/tribal/ cognizance@luke:~/Projects/tribal/ && ssh cognizance@luke "cd Projects/tribal/; scons"
 
-printf "Retrieving the executable from Luke and running."
-rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@Luke:~/Projects/tribal/ ~/Projects/tribal/
+printf "Retrieving the executable from Luke and running.\n"
+rsync -arvzSH --include="No-Motivation" --exclude="*" cognizance@luke:~/Projects/tribal/ ~/Projects/tribal/
 __GL_SYNC_TO_VBLANK=0 optirun ./No-Motivation
 
